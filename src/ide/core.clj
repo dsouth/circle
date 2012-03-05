c0f93d7405dffc97b5dade9dcd9c5cbff14db4a2
Doug South <doug.south@gmail.com>
2012-03-05 16:27:25 +1000

    keylistener for typing in a panel

--- src/ide/core.clj
+++ src/ide/core.clj
@@ -1,5 +1,23 @@
 (ns ide.core)
-(import '(java.awt.event KeyListener))
+(import
+ '(javax.swing JFrame JPanel)
+ '(java.awt.event KeyListener))
 
 (def keylistener
-  (proxy []))
+  (proxy [KeyListener] []
+    (keyTyped [event]
+      (println "keyTyped " event))
+    (keyPressed [event]
+      println "keyPressed " event)
+    (keyReleased [event]
+      println "keyReleased " event)))
+
+(def panel (JPanel.))
+
+(.addKeyListener panel keylistener)
+
+(def frame (JFrame.))
+(.setDefaultCloseOperation frame JFrame/DISPOSE_ON_CLOSE)
+(.add frame panel)
+(.pack frame)
+;;; (.setVisible frame true)
