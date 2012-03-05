3c32259a24c3d92cb351294e6948cd169af5f7fd
Doug South <doug.south@gmail.com>
2012-03-05 16:46:46 +1000

    simple key stuff

--- src/ide/core.clj
+++ src/ide/core.clj
@@ -1,23 +1,24 @@
 (ns ide.core)
 (import
  '(javax.swing JFrame JPanel)
- '(java.awt.event KeyListener))
+ '(java.awt.event KeyEvent  KeyListener))
 
 (def keylistener
   (proxy [KeyListener] []
     (keyTyped [event]
-      (println "keyTyped " event))
+      (println "keyTyped " (.getKeyChar event)))
     (keyPressed [event]
-      println "keyPressed " event)
+      println "keyPressed " (.getKeyChar event))
     (keyReleased [event]
-      println "keyReleased " event)))
+      println "keyReleased " (.getKeyChar event))))
 
 (def panel (JPanel.))
-
 (.addKeyListener panel keylistener)
 
 (def frame (JFrame.))
 (.setDefaultCloseOperation frame JFrame/DISPOSE_ON_CLOSE)
 (.add frame panel)
 (.pack frame)
+(.requestFocus panel) ;; perhaps on an expose listener?
+
 ;;; (.setVisible frame true)
