724371778d0a04af8d1fa34017f658097add69ac
Doug South <doug.south@gmail.com>
2012-03-05 14:59:29 +1000

    intial commit

---
+++ src/ide/core.clj
@@ -0,0 +1,5 @@
+(ns ide.core)
+(import '(java.awt.event KeyListener))
+
+(def keylistener
+  (proxy []))
---
+++ test/ide/test/core.clj
@@ -0,0 +1,6 @@
+(ns ide.test.core
+  (:use [ide.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
