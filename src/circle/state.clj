0b1bdc2e1678269755488da8b59477b7a268dab2
Doug South <doug.south@gmail.com>
2012-03-25 16:28:25 +1000

    moved loading file to state namespace

--- src/circle/state.clj
+++ src/circle/state.clj
@@ -4,3 +4,13 @@
 (def cursor-x (ref 0))
 ;; buffer is a vector of line vectors
 (def buffer (ref [[]]))
+
+(defn load-source [file]
+  (let [data (with-open [rdr (clojure.java.io/reader file)]
+               (doall (line-seq rdr)))
+        text (vec (map vec data))]
+    text))
+
+(defn load-buffer [file]
+  (dosync
+   (alter state/buffer utils/dummy (load-source file))))
