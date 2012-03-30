ffc141421bd4f1dee853a0c213bf4dd82d75a4d3
Doug South <doug.south@gmail.com>
2012-03-31 09:36:22 +1000

    moving tests to right ns

--- test/circle/test/edit.clj
+++ test/circle/test/edit.clj
@@ -55,34 +55,3 @@
            (vec "second")
            []
            (vec "third")])
-
-(facts "about moving the cursor"
-       (forward [(vec "first")] 0 0) => [0 1]
-       (forward [(vec "first")] 0 1) => [0 2]
-       (backward [(vec "first")] 0 5) => [0 4]
-       (backward [(vec "first")] 0 4) => [0 3]
-       (backward [(vec "first")] 0 1) => [0 0]
-       (up [(vec "fisrt") (vec "second")] 1 0) => [0 0]
-       (up [(vec "first") (vec "second")] 1 1) => [0 1]
-       (up [(vec "first") (vec "second")] 1 6) => [0 5]
-       (up [(vec "1")] 0 0) => [0 0]
-       (up [(vec "1") (vec "2") (vec "3")] 2 1) => [1 1]
-       (down [(vec "first") (vec "second")] 0 0) => [1 0]
-       (down [(vec "frist") (vec "second")] 0 1) => [1 1]
-       (down [(vec "long first") (vec "second")] 0 10) => [1 6]
-       (down [(vec "1")] 0 0) => [0 0]
-       (down [(vec "1") (vec "2") (vec "3")] 1 0) => [2 0])
-
-(facts "about moving past the end of the line"
-       (forward [(vec "first")
-                 (vec "second")] 0 5) => [1 0])
-
-(facts "about moving past the beginning of a line"
-       (backward [(vec "first")
-                  (vec "second")] 1 0) => [0 5])
-
-(facts "about not moving past the end of the document"
-       (forward [(vec "first")] 0 5) => [0 5])
-
-(facts "about moving past the beginning of the document"
-       (backward [(vec "first")] 0 0) => [0 0])
