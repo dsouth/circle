a88f714bf90c7ff56995b55ee56b2251b022086e
Doug South <doug.south@gmail.com>
2012-04-07 09:44:17 +1000

    add comments

--- src/circle/event.clj
+++ src/circle/event.clj
@@ -2,6 +2,7 @@
   (:require [circle.dispatch :as dispatch])
   (:import (java.awt.event KeyEvent KeyListener)))
 
+; Map from Java Swing events to applicaiton events
 (def key-map {KeyEvent/VK_BACK_SPACE :key-backspace
               KeyEvent/VK_LEFT       :key-left
               KeyEvent/VK_RIGHT      :key-right
@@ -10,6 +11,7 @@
 
 (def meta-map {KeyEvent/VK_L :gui-load-file})
 
+; meta map for modification key to Java Swing to application event map
 (def modifier-map {0 key-map, KeyEvent/META_MASK meta-map})
 
 (defn key-pressed [event]
@@ -20,13 +22,14 @@
           (do
             (dispatch/fire f nil)
             (.consume event))
+          ; Normal key presses don't have a function. Maybe they should??? :|
           (when (= m key-map)
             (dispatch/fire :key-typed (.getKeyChar event))
             (.consume event)))))))
 
+; Java interop stuff...
 (defn key-released [event])
 (defn key-typed [event])
-
 (def keylistener
   (proxy [KeyListener] []
     (keyTyped [event]
