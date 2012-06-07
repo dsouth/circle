2836b30649681bdcfc9204525100bcf932008737
Doug South <doug.south@gmail.com>
2012-06-08 09:34:40 +1000

    Gutting repl for renovations

--- src/circle/repl.clj
+++ src/circle/repl.clj
@@ -1,37 +1,10 @@
 (ns circle.repl
   (:require [circle.dispatch :as dispatch])
-  (:import (java.io Reader Writer)))
+  (:import (java.io PrintWriter PushbackReader Reader StringReader Writer)
+           (javax.swing SwingUtilities)))
 
-(defn fire-type [s]
-  (doseq [c s]
-    (dispatch/fire :key-typed c)))
-
-(def gui-writer
-  (proxy [Writer] []
-    (close [])
-    (flush [])
-    (write
-      ([a]
-         (if (instance? Long a)
-           (dispatch/fire :key-typed (char a))
-           (fire-type a)))
-      ([a b c]
-         (fire-type
-          (str "write:" a ", " b ", " c " where class is " (class a)))))))
-
-(def gui-reader
-  (proxy [Reader] []
-    (close [])
-    (mark [])
-    (markSupported [] false)
-    (read
-      ([])
-      ([char-a])
-      ([char-a off len]))
-    (ready [] false)
-    (reset [])
-    (skip [x])))
-
-;;; for testing
-;;; (binding [*out* gui-writer *in* *in*]
-;;;   (clojure.main/main))
+(defn read-string
+  "Given a String form, returns either a Clojrue data structure that
+for the String form or nil if the form is not valid."
+  [form]
+  (read (PushbackReader. (StringReader. form))))
