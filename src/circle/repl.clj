5c176e1ad0cf7bb46bc05addd0813aa94a5ab6e0
Doug South <doug.south@gmail.com>
2012-06-08 09:58:15 +1000

    moving repl forward

--- src/circle/repl.clj
+++ src/circle/repl.clj
@@ -1,10 +1,13 @@
 (ns circle.repl
-  (:require [circle.dispatch :as dispatch])
-  (:import (java.io PrintWriter PushbackReader Reader StringReader Writer)
-           (javax.swing SwingUtilities)))
+  (:require [circle.dispatch :as dispatch]
+            [circle.event :as event])
+  (:import (java.awt.event KeyEvent)
+           (java.io PushbackReader StringReader)))
 
 (defn read-string
   "Given a String form, returns either a Clojrue data structure that
 for the String form or nil if the form is not valid."
   [form]
   (read (PushbackReader. (StringReader. form))))
+
+(event/add-to-meta-map KeyEvent/VK_E :eval)
