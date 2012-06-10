b52dbd87cb82247ce86778f95f716c880fa7bd7d
Doug South <doug.south@gmail.com>
2012-06-10 10:02:53 +1000

    First cut of a somewhat working REPL

--- src/circle/repl.clj
+++ src/circle/repl.clj
@@ -1,8 +1,12 @@
 (ns circle.repl
   (:require [circle.dispatch :as dispatch]
-            [circle.event :as event])
+            [circle.event :as event]
+            [circle.core :as core])
   (:import (java.awt.event KeyEvent)
            (java.io PushbackReader StringReader)))
+
+(def from-x (ref 0))
+(def from-line (ref 0))
 
 (defn string->data
   "Given a String form, returns either a Clojrue data structure that
@@ -18,4 +22,28 @@ for the String form or nil if the form is not valid."
 
 (defn prompt []
   (display-string (str *ns*))
-  (display-string "=>"))
+  (display-string "=> ")
+  (dosync
+   (ref-set from-x (dispatch/receive :state-get-cursor-x))
+   (ref-set from-line (dispatch/receive :state-get-cursor-line))))
+
+(defn enter? [{modifier :modifier code :code}]
+  (and (= modifier 0)
+       (= code KeyEvent/VK_ENTER)))
+
+(defn do-the-repl []
+  (binding [*ns* *ns*]
+    (let [s (dispatch/receive :state-get-text-from {:x @from-x :line @from-line})
+          data (string->data s)
+          result (eval data)]
+      (display-string (str (if result result "nil") \newline))
+      (prompt))))
+
+(defn key-event [event]
+  (when (enter? event)
+    (do-the-repl)))
+
+(dispatch/add-reactor :key-event key-event)
+
+(core/main)
+(prompt)
