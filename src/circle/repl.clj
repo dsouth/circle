6fbc88433fbffc662ff19ffb4ed5e5e8de58828a
Doug South <doug.south@gmail.com>
2013-01-06 09:04:37 +1000

    REPL musings

--- src/circle/repl.clj
+++ src/circle/repl.clj
@@ -6,10 +6,35 @@
   (:import (java.awt.event KeyEvent)
            (java.io PushbackReader StringReader)))
 
-(defonce server (server/start-server :port 7888))
+(defn display-string [s]
+  (doseq [c s]
+    (dispatch/fire :key-typed c)))
 
-(defn eval [s]
+(defonce server (server/start-server :port 7888))
+(def from-x (ref 0))
+(def from-line (ref 0))
+
+(defn eval-string [s]
   (with-open [conn (repl/connect :port 7888)]
     (-> (repl/client conn 1000)
         (repl/message {:op "eval" :code s})
         repl/response-values)))
+
+(defn prompt []
+  (display-string "user=> ")
+  (dosync
+   (ref-set from-x (dispatch/receive :state-get-cursor-x))
+   (ref-set from-line (dispatch/receive :state-get-cursor-line))))
+
+(defn enter? [{modifier :modifier key :key}]
+  (and (= modifier 0)
+       (= key \newline)))
+
+(defn key-event [event]
+  (println event)
+  (when (enter? event)
+    (let [input (dispatch/receive :state-get-text-from {:x @from-x :line @from-line})
+          e-string (eval-string input)]
+      (println "input" input)
+      (println "evaluated to" e-string))
+    (prompt)))
