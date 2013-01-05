2d44745ccce6780db407520651baf2e90217ff77
Doug South <doug.south@gmail.com>
2013-01-05 17:05:31 +1000

    Use 1.5 RC1

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject circle "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [midje "1.3.1"]
-		 [com.stuartsierra/lazytest "1.2.3"]]
-  :repositories {"stuart" "http://stuartsierra.com/maven2"})
+  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+                 [org.clojure/tools.nrepl "0.2.0-RC1"]]
+                                        ; [midje "1.3.1"]]
+                                        ; [com.stuartsierra/lazytest "1.2.3"]]
+                                        ; :repositories {"stuart" "http://stuartsierra.com/maven2"})
