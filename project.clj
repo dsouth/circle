94295aa36d6ea268f9c96ef6676b418228d7ecf9
Doug South <doug.south@gmail.com>
2013-05-16 16:24:36 +1000

    deleting unused project dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,4 @@
 (defproject circle "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.5.0-RC1"]
-                 [org.clojure/tools.nrepl "0.2.0-RC1"]]
-                                        ; [midje "1.3.1"]]
-                                        ; [com.stuartsierra/lazytest "1.2.3"]]
-                                        ; :repositories {"stuart" "http://stuartsierra.com/maven2"})
-)
+                 [org.clojure/tools.nrepl "0.2.0-RC1"]])
