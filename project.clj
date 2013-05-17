92942aeb4eaf0f8ff30ffbf37e83517fbdd29fc5
Doug South <doug.south@gmail.com>
2013-05-17 22:46:12 +1000

    Perhaps it's time to start using 1.5.1...

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject circle "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.nrepl "0.2.0-RC1"]])
